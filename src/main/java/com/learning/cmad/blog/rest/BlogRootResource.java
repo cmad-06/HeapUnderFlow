d2b94f793597feebc49c6aa12b59cd9545c17aa2
cmad-06 <vibhor.jain@outlook.com>
2018-03-11 19:30:39 +0530

    added page for blog display

--- src/main/java/com/learning/cmad/blog/rest/BlogRootResource.java
+++ src/main/java/com/learning/cmad/blog/rest/BlogRootResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -30,4 +31,10 @@ public class BlogRootResource {
 		return Response.ok().entity(blogs).build();
 	}
 	
+	@GET
+    @Path("/{id}")
+	public Response getBlogById(@PathParam("id") int blogId) {
+		Blog blog = blogObj.getBlogById(blogId);
+		return Response.ok().entity(blog).build();
+	}
 }
