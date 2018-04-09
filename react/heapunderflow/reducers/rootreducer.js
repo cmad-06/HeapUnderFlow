cad67b82d75b1d269f5f27e1b0c32a8a9a3c3cbf
cadprasad <cadprasad@gmail.com>
2018-04-09 15:02:31 +0530

    Removing react old code

--- react/heapunderflow/reducers/rootreducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import {userReducer} from './userReducer'
-import {blogReducer} from './blogReducer'
-
-export default combineReducers({
-  user : userReducer,
-  blogs : blogReducer
-})
--- react/heapunderflow/store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from "../reducers/rootreducer.js";
-import logger from "../middleware/logger.js";
-import thunk from 'redux-thunk';
-
-const store = createStore(rootReducer, applyMiddleware(logger, thunk));
-const { getState, dispatch } = store;
-
-export default store;
