517f9cc75a0280be6ade1a2eebcc44832d2b78f2
vjcalling <vibhor.jain@outlook.com>
2018-04-12 09:47:24 +0530

    adding exception handling for comments

--- src/main/java/com/learning/cmad/comments/data/MorphiaCommentDAO.java
+++ src/main/java/com/learning/cmad/comments/data/MorphiaCommentDAO.java
@@ -3,10 +3,14 @@ package com.learning.cmad.comments.data;
 import java.util.List;
 
 import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Key;
 import org.mongodb.morphia.dao.BasicDAO;
 import org.mongodb.morphia.query.Query;
 
 import com.learning.cmad.comments.api.Comment;
+import com.learning.cmad.comments.api.CommentException;
+import com.learning.cmad.comments.api.CommentNotFoundException;
+import com.mongodb.WriteResult;
 
 public class MorphiaCommentDAO extends BasicDAO<Comment, String> implements CommentDAO {
 
@@ -20,7 +24,9 @@ public class MorphiaCommentDAO extends BasicDAO<Comment, String> implements Comm
 
 	@Override
 	public Comment createComment(Comment comment) {
-		save(comment);
+		Key<Comment> key = save(comment);
+		if(key == null)
+			throw new CommentException();
 		return comment;
 		
 	}
@@ -38,21 +44,29 @@ public class MorphiaCommentDAO extends BasicDAO<Comment, String> implements Comm
 	@Override
 	public Comment getCommentById(String id) {
 		Query<Comment> query = createQuery().field("commentId").equal(id);
-		return query.get();
+		Comment comment = query.get();
+		if(comment == null)
+			throw new CommentNotFoundException();
+		
+		return comment;
 	}
 
 //-----------------------------------------------------------------------------------------------------------
 	
 	@Override
 	public void updateComment(Comment comment) {
-		save(comment);
+		Key<Comment> key = save(comment);
+		if(key == null)
+			throw new CommentNotFoundException();
 	}
 
 //-----------------------------------------------------------------------------------------------------------
 	
 	@Override
 	public void deleteComment(Comment comment) {
-		delete(comment);
+		WriteResult result = delete(comment);
+		if(result == null)
+			throw new CommentNotFoundException();
 	}
 	
 //-----------------------------------------------------------------------------------------------------------
@@ -61,6 +75,10 @@ public class MorphiaCommentDAO extends BasicDAO<Comment, String> implements Comm
 	public void deleteCommentById(String id) {
 		Query<Comment> query = createQuery().field("commentId").equal(id);
 		Comment comment = query.get();
+		
+		if(comment == null)
+			throw new CommentNotFoundException();
+		
 		delete(comment);
 		
 	}
